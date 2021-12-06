1fa6497d7b48cedff329daf82c6f29c5176fc930
DwiFathanahWafi <dwwafii@gmail.com>
2021-12-06 16:31:12 +0700

    message

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'food',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'mysql',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
